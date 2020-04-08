6b60c2d9adf3832972ac8812725f36a321a6120e
teemuapu <49945450+teemuapu@users.noreply.github.com>
2020-04-08 12:03:13 +0300

    UiNavigaatio bugfix

---
+++ GameProject/Assets/Scripts/ButtonBugFix.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBugFix : MonoBehaviour
+{
+    public UnityEngine.UI.Button btn;
+    
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        btn.Select();
+    }
+
+}
