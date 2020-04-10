13e57b0aa72e9a51ab78b5645a5a49314097c7e0
Samuli Salin <samulsal@metropolia.fi>
2020-04-10 13:03:04 +0300

    CutsceneAndDialogTest-päivitetty

---
+++ GameProject/Assets/MysteeriScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteeriScript : MonoBehaviour
+{
+    public GameObject mysteeriMies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        mysteeriMies.SetActive(true);
+    }
+}
