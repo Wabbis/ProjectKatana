72444ed0860adbb6e7fb59264e58302d308130d8
Dragonath <dragonathplays@gmail.com>
2020-04-10 12:24:22 +0300

    merge error commit

---
+++ GameProject/Assets/Scripts/Cutscene/CutsceneInputManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneInputManager : MonoBehaviour
+{
+    public GameObject skipText;
+    private bool waitingForSkipInput;
+
+    private float skipTimer;
+    public float skipTimerMax;
+
+    public PlayableDirector timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitingForSkipInput = false;
+        timeline = GameObject.FindGameObjectWithTag("Timeline").GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (skipTimer < skipTimerMax && waitingForSkipInput)
+        {
+            skipTimer += Time.deltaTime;
+
+        }
+        else if (skipTimer >= skipTimerMax && waitingForSkipInput)
+        {
+            skipText.SetActive(false);
+
+            waitingForSkipInput = false;
+            skipTimer = 0;
+        }
+
+        if (Input.anyKeyDown && waitingForSkipInput)
+        {
+            SkipCutscene();
+
+        }
+        else if (Input.anyKeyDown && !waitingForSkipInput)
+        {
+            timeline = GameObject.FindGameObjectWithTag("Timeline").GetComponent<PlayableDirector>();
+            skipText.SetActive(true);
+            waitingForSkipInput = true;
+        }
+    }
+
+    public void SkipCutscene()
+    {
+        // Tee mitä ikinä tapahtuukaan kun käyttäjä skippaa cutscenen
+
+        Debug.Log("Cutscene skipped");
+        skipText.SetActive(false);
+        waitingForSkipInput = false;
+        skipTimer = 0;
+
+        timeline.time = timeline.playableAsset.duration;
+        timeline.Evaluate();
+        timeline.Stop();
+    }
+}
--- GameProject/Assets/Scripts/Cutscene/RevealText.cs
+++ GameProject/Assets/Scripts/Cutscene/RevealText.cs
@@ -2,43 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CutsceneLine
+{
+    public string line;             // Dialogin vuorosana
+    public Sprite characterImage;   // Puhuvan hahmon kuva
+    public float lineTime;          // Kuinka kauan teksti pysyy ruudussa
+
+    CutsceneLine()
+    {
+        line = "Default line";
+        characterImage = null;
+        lineTime = 0;
+    }
+}
 
 public class RevealText : MonoBehaviour
 {
-    public string[] lines;
-    private int linesCounter = 0;
+    public Image characterImageGameObject;
+    public TextMeshProUGUI text;
 
-    private TextMeshProUGUI text;
+    public CutsceneLine[] cutsceneLine;
+    private int linesCounter = 0;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        text = gameObject.GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
+        characterImageGameObject.sprite = cutsceneLine[linesCounter].characterImage;
 
-        int totalVisibleCharacters = lines[linesCounter].Length;
+        int totalVisibleCharacters = cutsceneLine[linesCounter].line.Length;
         int counter = 0;
 
         while (true)
         {
-
+            if (cutsceneLine[linesCounter].characterImage != null)
+            {
+                characterImageGameObject.sprite = cutsceneLine[linesCounter].characterImage;
+            }
             int visibleCount = counter % (totalVisibleCharacters + 1);
 
             text.maxVisibleCharacters = visibleCount;
-            text.text = lines[linesCounter];
+            text.text = cutsceneLine[linesCounter].line;
 
             if (visibleCount >= totalVisibleCharacters)
             {
-                yield return new WaitForSeconds(2.0f);
+                yield return new WaitForSeconds(cutsceneLine[linesCounter].lineTime);
+                counter = 0;
                 linesCounter++;
-
-                if (linesCounter >= lines.Length)
+                Debug.Log("Linescoutner: " + linesCounter + (" Linelenght: " + cutsceneLine.Length));
+                if (linesCounter >= cutsceneLine.Length)
                 {
                     yield return new WaitForSeconds(2.0f);
                     gameObject.SetActive(false);
                 }
                 else
                 {
-                    totalVisibleCharacters = lines[linesCounter].Length;
+                    totalVisibleCharacters = cutsceneLine[linesCounter].line.Length;
                 }
             }
 
