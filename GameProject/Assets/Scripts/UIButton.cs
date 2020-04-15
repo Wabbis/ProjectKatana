d388305d79a173af7bc3fefd0ddd6f9869dc5f5f
Petteri <petteri.vattulainen@metropolia.fi>
2020-04-15 11:25:16 +0300

    Volume sliders

---
+++ GameProject/Assets/Scripts/UIButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    Vector3 cachedScale;
+    public float expandScale;
+
+    void Start()
+    {
+
+        cachedScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        transform.localScale = new Vector3(expandScale, expandScale, expandScale);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+        transform.localScale = cachedScale;
+    }
+}
---
+++ GameProject/Assets/SetVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string target;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat(target, 0.75f);
+    }
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(target, Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat(target, sliderValue);
+    }
+}
