c347a1397741445228c7b6ce94af1719da87b343
Samuli Salin <samulsal@metropolia.fi>
2020-04-20 16:14:58 +0300

    Kamera-prefab, joka spawnaa turretteja lisätty. Kaikki kamera-prefabit päivitetty Enemy-layerille

---
+++ GameProject/Assets/TurretScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TurretScript : MonoBehaviour
+{
+    public float rateOfFire;       // Luotia sekunissa
+    public bool shooting;
+    public Animator animator;
+    public GameObject bullet;
+    public float bulletSpeed;
+    public GameObject bulletSpawn;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletSpawn.transform.right = player.transform.position - bulletSpawn.transform.position;
+    }
+
+    IEnumerator Shoot()
+    {
+        while (shooting)
+        {
+            Debug.Log(gameObject.transform.rotation.y);
+            if (gameObject.transform.rotation.y == 1 && gameObject.transform.position.x > player.transform.position.x || gameObject.transform.rotation.y == 0 &&  gameObject.transform.position.x < player.transform.position.x)
+            {
+                GameObject newBullet = Instantiate(bullet, bulletSpawn.transform.position, Quaternion.Euler(new Vector3(0, 0, bulletSpawn.transform.rotation.eulerAngles.z)));
+                Debug.Log(bulletSpawn.transform.rotation.z);
+                newBullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawn.transform.right * bulletSpeed * 0.0001f, ForceMode2D.Impulse);
+                Destroy(newBullet, 5);
+                yield return new WaitForSeconds(1f / rateOfFire);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    public void StartShooting()
+    {
+        shooting = true;
+        StartCoroutine(Shoot());
+    }
+}
