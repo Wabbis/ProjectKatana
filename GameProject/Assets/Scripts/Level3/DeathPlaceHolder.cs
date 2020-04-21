61f778b0450e5474cc29abb7576cb83c7ff6e78d
teemuapu <49945450+teemuapu@users.noreply.github.com>
2020-04-21 12:40:26 +0300

    Tämä on kartta

---
+++ GameProject/Assets/Scripts/Level3/DeathPlaceHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathPlaceHolder : MonoBehaviour
+{
+    
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        Debug.Log("Ded");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ GameProject/Assets/Scripts/Level3/Push.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    Vector3 startPosition;
+    Vector3 endPosition;
+    public bool push = false;
+    public bool back = false;
+    public int speed = 3;
+    public int counter = 0;
+    public GameObject palikka;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // endPosition = (startPosition.x - 5, startPosition.y, 4);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(push == true) {
+            counter++;
+            palikka.transform.Translate(Vector3.left * Time.deltaTime * speed);
+            if(counter > 120)
+            {
+                push = false;
+                counter = 0;
+                back = true;
+
+            }
+        }
+        if(back == true)
+        {
+            counter++;
+            palikka.transform.Translate(Vector3.right * Time.deltaTime * speed);
+            if (counter > 120)
+            {
+                back = false;
+                counter = 0;
+
+            }
+        }
+    }
+    public void SetPushTrue()
+    {
+        push = true;
+    }
+}
+
+
+//transform.position = Vector3.Lerp(startPosition, endPosition, speed* Time.deltaTime);
