d2bdee880c70240c0d561395d8a8a92ea47ce13d
Petteri <petteri.vattulainen@metropolia.fi>
2020-04-23 20:56:05 +0300

    level select menu

--- GameProject/Assets/Scripts/MainMenu.cs
+++ GameProject/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -13,26 +14,67 @@ public class MainMenu : MonoBehaviour
     public Button exitButton;
     public Button confirmExitButton;
     public Button cancelExitButton;
+    public Button continueButton;
+    public Button closeLevels;
 
     public GameObject menuPanel;
+    public GameObject levelPanel;
     public GameObject optionsPanel;
     public GameObject exitPanel;
+    public GameObject buttons;
+    public Button[] levelButtons;
+
+    public int firstLevelIndex;
+    public int numberOfLevels;
 
     // Start is called before the first frame update
     void Start()
     {
+        levelButtons = buttons.GetComponentsInChildren<Button>();
         optionsButton.onClick.AddListener(OpenOptions);
         closeOptionsButton.onClick.AddListener(CloseOptions);
         exitButton.onClick.AddListener(ExitGame);
         confirmExitButton.onClick.AddListener(ConfirmExit);
         cancelExitButton.onClick.AddListener(CancelExit);
+        continueButton.onClick.AddListener(OpenLevels);
+        closeLevels.onClick.AddListener(CloseLevels);
+
+        for(int i =firstLevelIndex;i<(firstLevelIndex+numberOfLevels);i++)
+        {
+            int t = i;
+            levelButtons[t].onClick.AddListener(()=>LoadLevel(t));
+        }
+
+
+       // foreach (Button b in levelButtons)
+       //     b.interactable = false;
+        //Activate();
+    }
+    void Activate(int s)
+    {
+        for(int i = 0; i < s; i++)
+        {
+            levelButtons[s].interactable = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadLevel(int l)
     {
-        
+        Debug.Log("button " + l);
+        //SceneManager.LoadScene(l);
     }
+ 
+    void OpenLevels()
+    {
+        menuPanel.gameObject.SetActive(false);
+        levelPanel.gameObject.SetActive(true);
+    }
+    void CloseLevels()
+    {
+        menuPanel.gameObject.SetActive(true);
+        levelPanel.gameObject.SetActive(false);
+    }
+  
     void OpenOptions()
     {
         menuPanel.gameObject.SetActive(false);
