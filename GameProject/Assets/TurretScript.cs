dc185fc5356f1ce385041f4d10b1897e83951c96
Samuli Salin <samulsal@metropolia.fi>
2020-04-27 10:43:09 +0300

    Turretit toimii taas

--- GameProject/Assets/TurretScript.cs
+++ GameProject/Assets/TurretScript.cs
@@ -34,11 +34,21 @@ public class TurretScript : MonoBehaviour
         while (shooting)
         {
             //Debug.Log(gameObject.transform.rotation.y);
-            if (gameObject.transform.rotation.y == 1 && gameObject.transform.position.x > player.transform.position.x || gameObject.transform.rotation.y == 0 &&  gameObject.transform.position.x < player.transform.position.x)
+            if (gameObject.transform.rotation.y == 1 && gameObject.transform.position.x > player.transform.position.x)
             {
                 GameObject newBullet = Instantiate(bullet, bulletSpawn.transform.position, Quaternion.Euler(new Vector3(0, 0, bulletSpawn.transform.rotation.eulerAngles.z)));
                 //Debug.Log(bulletSpawn.transform.rotation.z);
-                newBullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawn.transform.right * bulletSpeed * 0.0001f, ForceMode2D.Impulse);
+                newBullet.GetComponent<EnemyBullet>().dir = bulletSpawn.transform.right * -1;
+                //newBullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawn.transform.right * bulletSpeed * 0.0001f, ForceMode2D.Impulse);
+                Destroy(newBullet, 5);
+                yield return new WaitForSeconds(1f / rateOfFire);
+            }
+            else if(gameObject.transform.rotation.y == 0 && gameObject.transform.position.x < player.transform.position.x)
+            {
+                GameObject newBullet = Instantiate(bullet, bulletSpawn.transform.position, Quaternion.Euler(new Vector3(0, 0, bulletSpawn.transform.rotation.eulerAngles.z)));
+                //Debug.Log(bulletSpawn.transform.rotation.z);
+                newBullet.GetComponent<EnemyBullet>().dir = bulletSpawn.transform.right;
+                //newBullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawn.transform.right * bulletSpeed * 0.0001f, ForceMode2D.Impulse);
                 Destroy(newBullet, 5);
                 yield return new WaitForSeconds(1f / rateOfFire);
             }
