7d81c37e9468efd927d740fe2b74c9f416d23556
teemuapu <49945450+teemuapu@users.noreply.github.com>
2020-04-28 13:01:04 +0300

    WIP leveliin "hack" mekaniikka (mash E))

---
+++ GameProject/Assets/Scripts/Level22/UseComputer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseComputer : MonoBehaviour
+{
+    public bool canHack = false;
+    public bool dwindle = true;
+    public bool activate = false;
+
+    public Slider hackBar;
+
+    public float value = 0;
+    public float gainPerMash;
+    public int dwindleSpeed;
+    public GameObject placeHolderThing;
+    public GameObject placeHolderThing2;
+    public GameObject lightRed;
+    public GameObject lightGreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hackBar.value = 0.0f;
+    }
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        canHack = true;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        canHack = false;
+    }
+    void Update()
+    {
+        if(canHack == true)
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                value += gainPerMash;
+                hackBar.value = value;
+            }
+        }
+        if(value < 0.0f)
+        {
+            value = 0.0f;
+        }
+        if(value >= 1.00f)
+        {
+            dwindle = false;
+            activate = true;
+        }
+
+        
+    }
+    void FixedUpdate()
+    {
+        if(value > 0)
+        {
+            if((value - 0.01f) > 0.0f && dwindle == true)
+            {
+                value = value - (0.0001f * dwindleSpeed);
+                hackBar.value = value;
+            }
+        }
+        if(activate == true)
+        {
+            lightRed.SetActive(false);
+            lightGreen.SetActive(true);
+            //start cutscene
+            placeHolderThing.SetActive(false);
+            if(placeHolderThing2 != null)
+            {
+                placeHolderThing2.SetActive(true);
+            }
+        }
+    }
+}
