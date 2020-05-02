ccf96597846dc0906ce2ae69e73879727ffcae95
Dragonath <dragonathplays@gmail.com>
2020-05-02 14:12:13 +0300

    SoundManager + MusicManagerin alku

---
+++ GameProject/Assets/Scripts/Utilities/MusicManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static AudioClip ALARM, ARENEMY, PISTOLENEMY, EXPLODE, BOSSSHOT;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        ALARM = Resources.Load<AudioClip>("ALARM");
+        ARENEMY = Resources.Load<AudioClip>("ARENEMY");
+        PISTOLENEMY = Resources.Load<AudioClip>("PISTOLENEMY");
+        EXPLODE = Resources.Load<AudioClip>("EXPLODE");
+        BOSSSHOT = Resources.Load<AudioClip>("BOSSSHOT");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "ALARM":
+                audioSrc.Stop();
+                audioSrc.loop = true;
+                audioSrc.clip = ALARM;
+                audioSrc.Play();
+                break;
+            case "ARENEMY":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ARENEMY;
+                audioSrc.Play();
+                break;
+            case "PISTOLENEMY":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = PISTOLENEMY;
+                audioSrc.Play();
+                break;
+            case "EXPLODE":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = EXPLODE;
+                audioSrc.Play();
+                break;
+            case "BOSSSHOT":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = BOSSSHOT;
+                audioSrc.Play();
+                break;
+
+        }
+    }
+
+}
---
+++ GameProject/Assets/Scripts/Utilities/SoundManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip ALARM, ARENEMY, PISTOLENEMY, EXPLODE, BOSSSHOT, CHARGEUP, ELEVATORMOVE, ELEVATOROPEN, MELEESTAB, MENUHOVER, MENUSELECT, PICKUP, ROBOTAIM, ROBOTMOVE, SWITCH, TELEPORT;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        ALARM = Resources.Load<AudioClip>("ALARM");
+        ARENEMY = Resources.Load<AudioClip>("ARENEMY");
+        PISTOLENEMY = Resources.Load<AudioClip>("PISTOLENEMY");
+        EXPLODE = Resources.Load<AudioClip>("EXPLODE");
+        TELEPORT = Resources.Load<AudioClip>("TELEPORT");
+        BOSSSHOT = Resources.Load<AudioClip>("BOSSSHOT");
+        CHARGEUP = Resources.Load<AudioClip>("CHARGEUP");
+        ELEVATORMOVE = Resources.Load<AudioClip>("ELEVATORMOVE");
+        ELEVATOROPEN = Resources.Load<AudioClip>("ELEVATOROPEN");
+        MELEESTAB = Resources.Load<AudioClip>("MELEESTAB");
+        MENUHOVER = Resources.Load<AudioClip>("MENUHOVER");
+        MENUSELECT = Resources.Load<AudioClip>("MENUSELECT");
+        PICKUP = Resources.Load<AudioClip>("PICKUP");
+        ROBOTAIM = Resources.Load<AudioClip>("ROBOTAIM");
+        ROBOTMOVE = Resources.Load<AudioClip>("ROBOTMOVE");
+        SWITCH = Resources.Load<AudioClip>("SWITCH");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "ALARM":
+                audioSrc.Stop();
+                audioSrc.loop = true;
+                audioSrc.clip = ALARM;
+                audioSrc.Play();
+                break;
+            case "ARENEMY":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ARENEMY;
+                audioSrc.Play();
+                break;
+            case "PISTOLENEMY":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = PISTOLENEMY;
+                audioSrc.Play();
+                break;
+            case "EXPLODE":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = EXPLODE;
+                audioSrc.Play();
+                break;
+            case "TELEPORT":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = TELEPORT;
+                audioSrc.Play();
+                break;
+            case "BOSSSHOT":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = BOSSSHOT;
+                audioSrc.Play();
+                break;
+            case "CHARGEUP":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = CHARGEUP;
+                audioSrc.Play();
+                break;
+            case "ELEVATORMOVE":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ELEVATORMOVE;
+                audioSrc.Play();
+                break;
+            case "ELEVATOROPEN":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ELEVATOROPEN;
+                audioSrc.Play();
+                break;
+            case "MELEESTAB":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = MELEESTAB;
+                audioSrc.Play();
+                break;
+            case "MENUHOVER":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = MENUHOVER;
+                audioSrc.Play();
+                break;
+            case "MENUSELECT":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = MENUSELECT;
+                audioSrc.Play();
+                break;
+            case "PICKUP":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = PICKUP;
+                audioSrc.Play();
+                break;
+            case "ROBOTAIM":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ROBOTAIM;
+                audioSrc.Play();
+                break;
+            case "ROBOTMOVE":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = ROBOTMOVE;
+                audioSrc.Play();
+                break;
+            case "SWITCH":
+                audioSrc.Stop();
+                audioSrc.loop = false;
+                audioSrc.clip = SWITCH;
+                audioSrc.Play();
+                break;
+
+        }
+    }
+
+}
