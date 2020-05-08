f41d88ca21c86c981403e4038f44fb696ff6319b
Samuli Salin <samulsal@metropolia.fi>
2020-05-08 16:04:58 +0300

    World2 Level4 cutsceneja

---
+++ GameProject/Assets/Scripts/World2Level4/World2Level4Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World2Level4Manager : MonoBehaviour
+{
+    public GameObject gameManagerObject;
+    public GameManager gameManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerObject = UnityEngine.GameObject.Find("GameManager") ?? Instantiate(gameManagerObject, new Vector3(0, 0, 0), Quaternion.identity);
+        gameManagerScript = gameManagerObject.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EndLevel()
+    {
+        gameManagerScript.LoadNextScene();
+    }
+}
