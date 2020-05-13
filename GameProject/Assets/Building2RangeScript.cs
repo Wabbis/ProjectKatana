364f3db59b60fe9e99ca1c6caaa183cc9e7f6ea2
Samuli Salin <samulsal@metropolia.fi>
2020-05-13 20:31:19 +0300

    Bugikorjauksia

---
+++ GameProject/Assets/Building2RangeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building2RangeScript : MonoBehaviour
+{
+    public EnemyHealth enemyHealth;
+    public PolygonCollider2D polygonCollider;
+    public Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyHealth = gameObject.GetComponent<EnemyHealth>();
+        polygonCollider = gameObject.GetComponent<PolygonCollider2D>();
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyHealth.health == 0)
+        {
+            polygonCollider.enabled = false;
+            rigidbody2d.isKinematic = true;
+        }
+    }
+}
